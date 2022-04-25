f9223da156de62fcd3360f26a9d33bbca50e28a7
WitaminF <scheiblerf@gmail.com>
2022-04-25 11:21:11 +0200

    avancée cours no2

--- Cours_2/index.js
+++ Cours_2/index.js
@@ -14,3 +14,25 @@
 //baliseHTML.style.background = "yellow";
 
 //Click event
+const questionContainer = document.querySelector(".click-event")
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
+
+
+questionContainer.addEventListener("click", ()=>{
+    questionContainer.classList.toggle("question-click");
+});
+
+btn1.addEventListener("click", () => {
+    response.classList.add("show-response");
+    response.style.background = "green";
+});
+
+btn2.addEventListener("click", () => {
+    response.classList.add("show-response");
+    response.style.background = "red";
+});
+
+
+//----------------------------
