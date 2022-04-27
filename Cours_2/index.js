2ed0b558ceb9432d9b2d249ca71a11d5ecc1f74a
WitaminF <scheiblerf@gmail.com>
2022-04-27 10:47:03 +0200

    fin de partie du cours sur les scroll event, début de la partie form event

--- Cours_2/index.js
+++ Cours_2/index.js
@@ -91,6 +91,22 @@ document.addEventListener("keypress", (e)=>{
     ring(e.key);
 });
 
+// Scroll event --------------------------------------------------------
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", ()=>{
+    console.log(window.scrollY);
+
+    if (window.scrollY > 120){
+        nav.style.top = 0;
+    } else {
+        nav.style.top = "-50px";
+    }
+})
+
+// Form event ---------------------------------------------------------
+
 
 
 
