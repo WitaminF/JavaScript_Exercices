ba03f91f5ec3735505d4ac38d292babc9ba97795
WitaminF <scheiblerf@gmail.com>
2022-04-27 11:31:57 +0200

    fin de la partie du cours sur les form event, début load event

--- Cours_2/index.js
+++ Cours_2/index.js
@@ -88,7 +88,9 @@ document.addEventListener("keypress", (e)=>{
         keypressContainer.style.background = "red";
     }
 
+    if (e.key === "z"){
     ring(e.key);
+    }
 });
 
 // Scroll event --------------------------------------------------------
@@ -96,7 +98,6 @@ document.addEventListener("keypress", (e)=>{
 const nav = document.querySelector("nav");
 
 window.addEventListener("scroll", ()=>{
-    console.log(window.scrollY);
 
     if (window.scrollY > 120){
         nav.style.top = 0;
@@ -106,6 +107,37 @@ window.addEventListener("scroll", ()=>{
 })
 
 // Form event ---------------------------------------------------------
+
+const inputName = document.querySelector('input[type="text"]');
+const select = document.querySelector("select");
+const form = document.querySelector("form");
+let pseudo = "";
+let language = "";
+
+
+
+inputName.addEventListener("input", (e)=>{
+    pseudo = e.target.value;
+
+});
+
+select.addEventListener("input", (e)=>{
+    language = e.target.value;
+});
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    if (cgv.checked){
+        document.querySelector("form > div").innerHTML =
+            `<h3>Pseudo : ${pseudo}</h3> <h4> Langage préfére : ${language} </h4>`;
+    } else {
+        alert("Veuillez accepter les CGV");
+    }
+});
+
+
+
 
 
 
