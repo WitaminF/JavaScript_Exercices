a01a5427459cb371ce6240b15313c38c1895eb2e
WitaminF <scheiblerf@gmail.com>
2022-05-25 09:23:24 +0200

    avancement cours no3

---
+++ Cours_3/Dossier_de_cours/script.js
@@ -0,0 +1,32 @@
+//Tableau
+let array = [
+    {prenom: "Florent", nom: "Scheibler"},
+    {prenom: "Maeva", nom: "Wodege"},
+    {prenom: "Arnaud", nom: "Ehrsam"}
+];
+
+let data = [{
+    pseudo:"Witamin",
+    age: 29,
+    technos:["Javascript", "ELO", "React"],
+    admin: false
+    },
+    {
+        pseudo:"Poussin53",
+        age: 36,
+        technos:["Php", "ELO", "Laravel"],
+        admin: false
+    },
+    {
+        pseudo:"Maevawo",
+        age: 42,
+        technos:["Sql", "NodeJS", "React"],
+        admin: true
+    }
+]
+
+//-----------------------
+//Structures de contrôles
+//-----------------------
+//else if, while, do while etc..
+
