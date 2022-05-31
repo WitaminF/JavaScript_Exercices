106de8872feae21ab3935f718607445bf0427029
WitaminF <scheiblerf@gmail.com>
2022-05-31 11:35:53 +0200

    fin de la partie sur fetch, début partie asyncrone

--- Cours_4/Dossier_de_cours/index.js
+++ Cours_4/Dossier_de_cours/index.js
@@ -42,3 +42,22 @@ const init = {
 //fetch("data.json", init).then((res) => console.log(res));
 //--------------------------------------------------------------
 //CRUD : Create (POST), Read (GET), Update (PUT), Delete (DELETE)
+
+const init2 = {
+    method: "POST" ,
+    headers:{
+        "Content-Type": "application/json"
+    },
+    body:JSON.stringify({
+        pseudo: "From Scratch",
+        message: "Salut les gens"
+    }),
+    mode : "cors",
+    credentials: "same-origin"
+};
+document.querySelector("form").addEventListener("submit", ()=>{
+    fetch("http://localhost:3000/posts", init2).then(()=>
+        console.log("data envoyée"));
+})
+
+//-----------------------
