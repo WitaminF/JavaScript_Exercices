6bc40a13d413122418cbcac29c8d5f4ce780e429
WitaminF <scheiblerf@gmail.com>
2022-05-31 08:28:52 +0200

    début du projet joke-app

---
+++ Cours_4/Dossier_de_cours/index.js
@@ -0,0 +1,28 @@
+//XMLHttpRequest
+/*function reqListener() {
+    console.log(this.responseText);
+}
+
+let req = new XMLHttpRequest();
+req.onload = reqListener;
+//req.open("get", "data.txt", true);
+//req.open("get", "data.json", true);
+//req.open("get", "https://api.blablagues.net/?rub=blagues", true);
+req.send();*/
+
+//---------------------------
+//FETCH
+//---------------------------
+
+//Fetch lien + option, then : tant que tu n'as pas la réponse (promesse)
+//fetch("monlien", "object d'options").then((response)=>{
+    //response
+    //Si la requête n'est pas passée, on fait un catch pour l'erreur
+//}).catch((err) => console.log(err));
+
+fetch("data.txt")
+    .then((res) => res.text());
+    //.then((data) => console.log(data));
+fetch("data.json")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
